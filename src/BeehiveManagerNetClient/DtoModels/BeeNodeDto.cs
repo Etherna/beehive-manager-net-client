356e04648c25ac4152c7f2792c79f4b51ac44268
Mirko Da Corte <mirko@etherna.io>
2022-05-22 01:29:01 +0200

    update api

--- src/BeehiveManagerNetClient/DtoModels/BeeNodeDto.cs
+++ src/BeehiveManagerNetClient/DtoModels/BeeNodeDto.cs
@@ -25,23 +25,25 @@ namespace Etherna.BeehiveManager.NetClient.DtoModels
                 throw new ArgumentNullException(nameof(beeNode));
 
             Id = beeNode.Id!;
+            ConnectionScheme = beeNode.ConnectionScheme;
             DebugPort = beeNode.DebugPort;
             EthereumAddress = beeNode.EthereumAddress;
             GatewayPort = beeNode.GatewayPort;
+            Hostname = beeNode.Hostname;
             OverlayAddress = beeNode.OverlayAddress;
             PssPublicKey = beeNode.PssPublicKey;
             PublicKey = beeNode.PublicKey;
-            Url = beeNode.Url!;
         }
 
         // Properties.
         public string Id { get; }
-        public int? DebugPort { get; }
+        public string ConnectionScheme { get; }
+        public int DebugPort { get; }
         public string? EthereumAddress { get; }
-        public int? GatewayPort { get; }
+        public int GatewayPort { get; }
+        public string Hostname { get; }
         public string? OverlayAddress { get; }
         public string? PssPublicKey { get; }
         public string? PublicKey { get; }
-        public Uri Url { get; }
     }
 }
